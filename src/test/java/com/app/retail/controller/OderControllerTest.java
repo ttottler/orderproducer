495b2bca8f63d33ec284ec1f8e7ad7ed83c8cf8e
karthick <ttottler@gmail.com>
2022-07-13 17:29:01 +0530

    test case

--- src/test/java/com/app/retail/controller/OderControllerTest.java
+++ src/test/java/com/app/retail/controller/OderControllerTest.java
@@ -7,9 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.app.retail.model.Order;
